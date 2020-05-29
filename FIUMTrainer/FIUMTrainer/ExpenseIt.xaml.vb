aa704d51eba2b55d3460878cca6dc2cfbfd2bf22
sentinelia <lucasjumepi@gmail.com>
2020-05-29 14:34:05 -0300

    Fixed wrong upload

--- FIUMTrainer/FIUMTrainer/ExpenseIt.xaml.vb
+++
@@ -1,5 +0,0 @@
-﻿Class ExpenseIt
-    Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
-        NavigationService.Navigate(New Uri("am1.xaml", UriKind.Relative))
-    End Sub
-End Class
--- FIUMTrainer/FIUMTrainer/am1.xaml.vb
+++
@@ -1,5 +0,0 @@
-﻿Class am1
-    Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
-        NavigationService.Navigate(New Uri("teoricoam1.xaml", UriKind.Relative))
-    End Sub
-End Class
