f2872ccc42a13e14caa36897ade894a87609bba5
ElLucasM <lucasjumepi@gmail.com>
2020-05-29 14:25:31 -0300

    Add files via upload

---
+++ FIUMTrainer/FIUMTrainer/am1.xaml.vb
@@ -0,0 +1,5 @@
+﻿Class am1
+    Private Sub button_Click(sender As Object, e As RoutedEventArgs) Handles button.Click
+        NavigationService.Navigate(New Uri("teoricoam1.xaml", UriKind.Relative))
+    End Sub
+End Class
---
+++ FIUMTrainer/FIUMTrainer/teoricoam1.xaml.vb
@@ -0,0 +1,10 @@
+﻿Class teoricoam1
+    Private Sub treeView_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles treeView.MouseDoubleClick
+        Select Case treeView.SelectedItem.Header
+            Case "Definidas"
+                webBrowser1.Navigate(New Uri("C:\Users\Lucas J. Melogno\source\repos\FIUMTrainer\FIUMTrainer\Resources\definidas.xps"))
+            Case "Impropias"
+                webBrowser1.Navigate(New Uri("C:\Users\Lucas J. Melogno\source\repos\FIUMTrainer\FIUMTrainer\Resources\impropias.xps"))
+        End Select
+    End Sub
+End Class
